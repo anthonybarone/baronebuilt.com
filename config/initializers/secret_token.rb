aad8a2b3af97f5dada30fe643c8a6c95010db777
anthonybarone <barone42@gmail.com>
2011-11-07 23:21:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaronebuiltCom::Application.config.secret_token = '5f09c9565bfce63b677aee99a0742fbe21180881f89f3a7ced87d6362b0376be5bec1e3b47cc5a227293c5a45f225bf52945f3a6005438e2cbaafed63347aa6b'
